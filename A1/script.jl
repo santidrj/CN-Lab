7a17b3816c4471cdf25adad70fb9f6f1e64d6ac1
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2022-02-25 09:02:11 +0100

    Add average path length

--- A1/script.jl
+++ A1/script.jl
@@ -5,18 +5,21 @@ Pkg.add(name = "GraphIO", version = "0.5.0")
 Pkg.add("PrettyTables")
 
 using LightGraphs, GraphIO, Printf, PrettyTables
+using Statistics
 
-function GetFileName(filename)
+function get_file_name(filename)
     return filename[begin:findlast(isequal('.'), filename)-1]
 end
 
-header = (["Network", "#nodes", "#edges", "Max degree", "Min degree", "Avg. degree"])
-table = transpose(Vector{Float64}(undef, 6))
+header = (["Network", "#nodes", "#edges", "Max degree", "Min degree", "Avg. degree", "Avg. cluster coefficient", "Avg. path length"])
+table = transpose(Vector{Float64}(undef, length(header)))
 
 for (root, dirs, files) in walkdir("A1-networks")
     for file in files
         g = loadgraph(joinpath(root, file), NETFormat())
-        row = [GetFileName(file) nv(g) ne(g) Δ(g) δ(g) (ne(g) / nv(g))]
+        f(x) = gdistances(g, x)
+        path_lengths = reduce(hcat, f.(vertices(g)))
+        row = [get_file_name(file) nv(g) ne(g) Δ(g) δ(g) (ne(g) / nv(g)) mean(local_clustering_coefficient(g)) mean(path_lengths)]
         global table = [table; row]
     end
 end
