e1e4ca2f791b6d3557a06b2a932844084f030b40
Yazmina <yazmina.zurita@gmail.com>
2022-02-26 15:57:12 +0100

    Round decimals to 4 digits

--- A1/script.jl
+++ A1/script.jl
@@ -24,7 +24,7 @@ for (root, dirs, files) in walkdir("A1-networks")
         g = loadgraph(joinpath(root, file), NETFormat())
         f(x) = gdistances(g, x)
         path_lengths = reduce(hcat, f.(vertices(g)))
-        row = [get_file_name(file) nv(g) ne(g) Δ(g) δ(g) (ne(g) / nv(g)) mean(local_clustering_coefficient(g)) assortativity(g) mean(path_lengths) maximum(path_lengths)]
+        row = [get_file_name(file) nv(g) ne(g) Δ(g) δ(g) round((ne(g) / nv(g)), digits=4) round(mean(local_clustering_coefficient(g)), digits=4) round(assortativity(g), digits=4) round(mean(path_lengths), digits=4) maximum(path_lengths)]
         global table = [table; row]
     end
 end
@@ -34,4 +34,4 @@ open("results/networks_descriptors_latex.txt", "w") do f
     pretty_table(f, table; backend = Val(:latex), header = header)
 end
 
-CSV.write("results/networks_descriptors.csv", DataFrame(table, :auto), header = header, delimiter) 
+CSV.write("results/networks_descriptors.csv", DataFrame(table, :auto), header = header) 
