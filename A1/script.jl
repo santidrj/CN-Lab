7a9d7590ffd63edf5082bc9ea26d2b9d66ec64c2
Santiago Del Rey <59149491+santidrj@users.noreply.github.com>
2022-02-26 16:05:24 +0100

    Fix unwanted row appearing at the table

--- A1/script.jl
+++ A1/script.jl
@@ -6,7 +6,9 @@ Pkg.add("PrettyTables")
 Pkg.add("CSV")
 Pkg.add("DataFrames")
 
-using LightGraphs, GraphIO, Printf, PrettyTables
+using LightGraphs
+using GraphIO
+using PrettyTables
 using Statistics
 using CSV
 using DataFrames
@@ -17,15 +19,21 @@ function get_file_name(filename)
 end
 
 header = (["Network", "#nodes", "#edges", "Max degree", "Min degree", "Avg. degree", "Avg. cluster coefficient", "Assortativity", "Avg. path length", "Diameter"])
-table = transpose(Vector{Float64}(undef, length(header)))
+table = []
 
+first = true
 for (root, dirs, files) in walkdir("A1-networks")
     for file in files
         g = loadgraph(joinpath(root, file), NETFormat())
         f(x) = gdistances(g, x)
         path_lengths = reduce(hcat, f.(vertices(g)))
         row = [get_file_name(file) nv(g) ne(g) Δ(g) δ(g) (ne(g) / nv(g)) mean(local_clustering_coefficient(g)) assortativity(g) mean(path_lengths) maximum(path_lengths)]
-        global table = [table; row]
+        if first
+            global table = row
+            global first = false
+        else
+            global table = [table; row]
+        end
     end
 end
 
@@ -34,4 +42,4 @@ open("results/networks_descriptors_latex.txt", "w") do f
     pretty_table(f, table; backend = Val(:latex), header = header)
 end
 
-CSV.write("results/networks_descriptors.csv", DataFrame(table, :auto), header = header, delimiter) 
+CSV.write("results/networks_descriptors.csv", DataFrame(table, :auto), header = header);
