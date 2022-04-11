2d36616ef2f63047589cac201edf81975f496e69
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2022-04-11 19:44:38 +0200

    Refactor metrics.sh

--- A3/metrics.sh
+++ A3/metrics.sh
@@ -1,19 +1,16 @@
 #!/usr/bin/bash
 
-echo "Running"
 files="A3-networks/**/*.clu"
 algs=("louvain")
 tmpfile="temporari.csv"
-for alg in $algs; do
-    for f in $files; do
-        echo "Processing $f file..."
-        filename=$(basename $f .clu)
-        net=${filename%-*}
-        partition=`find nets/ -name ${net}*`
-        # echo $filename
-        # echo $net
-        # echo "Partition $partition"
-        if [[ "rb125" == "$net" ]]; then
+for f in $files; do
+    echo "Processing $f ..."
+    filename=$(basename $f .clu)
+    net=${filename%-*}
+    partition=`find nets/ -name ${net}*`
+    first=true
+    for alg in $algs; do
+        if [ "rb125" = "$net" ]; then
             number=${filename#*-}
             outfile="results/${net}-${number}_${alg}.csv"
         else
@@ -21,7 +18,13 @@ for alg in $algs; do
         fi
 
         ./Compare_partitions $partition $f $tmpfile t
-        tr '\t' ',' < $tmpfile > $outfile
+        # tr '\t' ',' < $tmpfile | awk -F',' '{print $11 $16 $26}'> $outfile
+        if [ "$first" = true ]; then
+            cut -f 1,11,16,26 $tmpfile | tr '\t' ',' > $outfile
+            first=false
+        else
+            tail -n 1 $tmpfile | cut -f 1,11,16,26 | tr '\t' ',' >> $outfile
+        fi
     done
 done
 rm $tmpfile
