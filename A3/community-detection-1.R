5176b67f7fc0f1a28d669f953f5f01b21abfde30
Santiago Del Rey <59149491+santidrj@users.noreply.github.com>
2022-04-14 10:50:56 +0200

    Fix problem when generating result tables

--- A3/community-detection-1.R
+++ A3/community-detection-1.R
@@ -21,58 +21,84 @@ get.reference <- function(file) {
 }
 
 for (f in list.files(file.path("A3-networks"),
+                     pattern = "*.net",
                      recursive = TRUE,
                      full.names = TRUE)) {
   file.name <- basename(f)
-  if (file_ext(file.name) == "net") {
-    net.name <- file_path_sans_ext(file.name)
-    cat("Network: ", net.name, "\n")
-    
-    g <- read.graph(f, format = "pajek")
-    
-    if (all(c("x", "y") %in% vertex_attr_names(g))) {
-      cat("The network has coordinates\n")
+  net.name <- file_path_sans_ext(file.name)
+  cat("Network: ", net.name, "\n")
+  
+  g <- read.graph(f, format = "pajek")
+  
+  if (all(c("x", "y") %in% vertex_attr_names(g))) {
+    cat("The network has coordinates\n")
+  }
+  
+  
+  aux.df <- data.frame(matrix(nrow = 0, ncol = 2))
+  colnames(aux.df) <- c("File", "Modularity")
+  old.modularity <- data.frame(matrix(nrow = 0, ncol = 3))
+  colnames(old.modularity) <-
+    c("Partition", "Modularity", "Reference Modularity")
+  
+  pat <- sprintf("^%s.*\\.clu$", gsub("\\+", "\\\\+", net.name))
+  ref.modularity <- "-"
+  modularity.file <- net.name
+  for (ref.cluster in list.files(dirname(f), pattern = pat)) {
+    if (grepl("rb125", net.name, fixed = T)) {
+      modularity.file <- file_path_sans_ext(ref.cluster)
     }
     
+    ref <- scan(
+      file.path(dirname(f), ref.cluster),
+      what = integer(),
+      skip = 1,
+      quiet = T
+    )
+    ref.modularity <- round(modularity(g, ref + 1), digits = 4)
+    aux.df[nrow(aux.df) + 1, ] <-
+      c(modularity.file, ref.modularity)
+    out.table <- file.path(
+        "results",
+        paste(modularity.file, "_modularity", ".csv", sep = "")
+      )
     
-    aux.df <- data.frame(matrix(nrow = 0, ncol = 2))
-    colnames(aux.df) <- c("File", "Modularity")
-    pat <- sprintf("^%s.*\\.clu$", net.name)
-    ref.modularity <- "-"
-    modularity.file <- net.name
-    for (ref.cluster in list.files(dirname(f), pattern = pat)) {
-      if (grepl("rb125", net.name, fixed = T)) {
-        modularity.file <- file_path_sans_ext(ref.cluster)
+    df <- data.frame(matrix(nrow = 0, ncol = 3))
+    colnames(df) <-
+      c("Partition", "Modularity", "Reference Modularity")
+    
+    if (file.exists(out.table)) {
+      # write("\n",file=out.table,append=TRUE)
+      old.modularity <- read.csv(out.table)
+      if (any(old.modularity$Partition == "Girvan-Newman")) {
+        df[nrow(df) + 1,] <- old.modularity[old.modularity$Partition == "Girvan-Newman",]
       }
-      
-      ref <- scan(
-        file.path(dirname(f), ref.cluster),
-        what = integer(),
-        skip = 1,
-        quiet = T
-      )
-      ref.modularity <- round(modularity(g, ref + 1), digits = 4)
-      aux.df[nrow(aux.df) + 1, ] <-
-        c(modularity.file, ref.modularity)
-      df <- data.frame(matrix(nrow = 0, ncol = 3))
-      colnames(df) <-
-        c("Partition", "Modularity", "Reference Modularity")
-      write.table(
-        df,
-        file = file.path(
-          "results",
-          paste(modularity.file, "_modularity", ".csv", sep = "")
-        ),
-        sep = ",",
-        quote = F,
-        row.names = F
-      )
     }
+    
+    write.table(
+      df,
+      file = out.table,
+      sep = ",",
+      quote = F,
+      row.names = F
+    )
     
     if (is.character(ref.modularity)) {
+      out.table <- file.path(
+          "results",
+          paste(modularity.file, "_modularity", ".csv", sep = "")
+        )
       df <- data.frame(matrix(nrow = 0, ncol = 3))
       colnames(df) <-
         c("Partition", "Modularity", "Reference Modularity")
+      
+      if (file.exists(out.table)) {
+        # write("\n",file=out.table,append=TRUE)
+        old.modularity <- read.csv(out.table)
+        if (any(old.modularity$Partition == "Girvan-Newman")) {
+          df[nrow(df) + 1,] <- old.modularity[old.modularity$Partition == "Girvan-Newman",]
+        }
+      }
       write.table(
         df,
         file = file.path(
