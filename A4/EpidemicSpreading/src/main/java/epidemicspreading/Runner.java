232806caacb187630ffda0724153819f12bbb989
Yazmina <yazmina.zurita@gmail.com>
2022-04-25 07:04:21 +0200

    Include mu in the folder name of the results

--- A4/EpidemicSpreading/src/main/java/epidemicspreading/Runner.java
+++ A4/EpidemicSpreading/src/main/java/epidemicspreading/Runner.java
@@ -79,7 +79,7 @@ public class Runner {
                 break;
             case "ER":
                 graph = GenerateGraph.ER(N, p);
-                fn = String.format("%s-%d-%f", network, N, p);
+                fn = String.format("%s-%d-%.f", network, N, p);
                 break;
         }
 
@@ -101,7 +101,8 @@ public class Runner {
         System.out.println("Done");
 
         // Save results
-        String resultsDir = Paths.get(resultsPath, fn).toString();
+        String resultsDir = Paths.get(resultsPath, fn+String.format("%.f"), mu)
+                            .toString();
         File f = new File(resultsDir);
         f.mkdir();
 
