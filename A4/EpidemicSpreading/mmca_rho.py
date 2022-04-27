73fd1b62f35791e4166693b6abfe09201c5ead1a
Santiago del Rey <santiago.del.rey@greenmlchess.essi.upc.edu>
2022-04-27 14:04:14 +0200

    Upload mmcaRho

--- A4/EpidemicSpreading/mmca_rho.py
+++ A4/EpidemicSpreading/mmca_rho.py
@@ -28,14 +28,16 @@ for root, dirs, files in os.walk(networks_path):
         adj_matrix = nx.to_numpy_array(G)
         net_dir = os.path.join(results_path, net_name)
         for subdir in os.listdir(net_dir):
-            mu = float(subdir.split("-")[-1])
-            print(f'\r{LINE_CLEAR} mu = {mu}')
+            save_path = os.path.join(net_dir, subdir, "mmcaRho.txt")
             beta = np.loadtxt(os.path.join(net_dir, subdir, "beta.txt"))
-            n_beta = beta.shape[0]
-            rho = np.empty(n_beta)
-            for i in range(n_beta):
-                rho[i] = mmca(adj_matrix, 0.2, beta[i], mu, 1000, 900)
-            np.savetxt(os.path.join(net_dir, subdir, "mmcaRho.txt"), rho)
+            if not os.path.exists(save_path) or np.all(np.isnan(np.loadtxt(save_path))) or len(beta) != len(np.loadtxt(save_path)):
+                mu = float(subdir.split("-")[-1])
+                print(f'\r{LINE_CLEAR} mu = {mu}')
+                n_beta = beta.shape[0]
+                rho = np.empty(n_beta)
+                for i in range(n_beta):
+                    rho[i] = mmca(adj_matrix, 0.2, beta[i], mu, 1000, 900)
+                np.savetxt(os.path.join(net_dir, subdir, "mmcaRho.txt"), rho)
 
 
 
