aef4e3b46e668c3b4cef668fba361e25d6a73494
Yazmina <yazmina.zurita@gmail.com>
2022-04-27 08:45:25 +0200

    Implement mmcaRho computation

--- A4/EpidemicSpreading/mmca_rho.py
+++ A4/EpidemicSpreading/mmca_rho.py
@@ -21,20 +21,19 @@ def mmca(adj_matrix, p0, beta, mu, t_max, t_trans):
 for root, dirs, files in os.walk(networks_path):
     for filename in files:
         net_name, ext = os.path.splitext(filename)
-        if net_name == "BA-500-5-3-6" and ext == ".net":
-            print(net_name)
-            G = nx.read_pajek(os.path.join(root, filename))
-            adj_matrix = nx.to_numpy_array(G)
-            net_dir = os.path.join(results_path, net_name)
-            for subdir in os.listdir(net_dir):
-                mu = float(subdir.split("-")[-1])
-                print(f'\r mu = {mu}')
-                beta = np.loadtxt(os.path.join(net_dir, subdir, "beta.txt"))
-                n_beta = beta.shape[0]
-                rho = np.empty(n_beta)
-                for i in range(n_beta):
-                    rho[i] = mmca(adj_matrix, 0.2, beta[i], mu, 1000, 900)
-                np.savetxt(os.path.join(net_dir, subdir, "mmcaRho.txt"), rho)
+        print(net_name)
+        G = nx.read_pajek(os.path.join(root, filename))
+        adj_matrix = nx.to_numpy_array(G)
+        net_dir = os.path.join(results_path, net_name)
+        for subdir in os.listdir(net_dir):
+            mu = float(subdir.split("-")[-1])
+            print(f'\r mu = {mu}')
+            beta = np.loadtxt(os.path.join(net_dir, subdir, "beta.txt"))
+            n_beta = beta.shape[0]
+            rho = np.empty(n_beta)
+            for i in range(n_beta):
+                rho[i] = mmca(adj_matrix, 0.2, beta[i], mu, 1000, 900)
+            np.savetxt(os.path.join(net_dir, subdir, "mmcaRho.txt"), rho)
 
 
 
