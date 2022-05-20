787a3d5c480589bde7bd08c06dd91c2a749f14dc
Santiago del Rey <59149491+santidrj@users.noreply.github.com>
2022-05-20 09:12:23 +0200

    Fix issues in creation of the network
    We remove duplicated lines and self-loops created when manipulating the
    data.

--- Project/create_network.py
+++ Project/create_network.py
@@ -2,6 +2,7 @@ import os
 
 import geopandas
 import networkx as nx
+import pandas as pd
 from matplotlib import pyplot as plt
 from networkx import DiGraph
 
@@ -14,6 +15,7 @@ print(net_data.head())
 relevant_columns = ['NOM_PARADA', 'ORDRE', 'NOM_LINIA', 'SENTIT', 'geometry']
 
 net_data = net_data[relevant_columns]
+net_data.drop_duplicates(inplace=True)
 
 net_data_grouped = net_data.groupby(['NOM_LINIA', 'SENTIT'], sort=False).apply(
     lambda g: g.sort_values(by='ORDRE', ascending=True)).reset_index(drop=True)
@@ -26,18 +28,25 @@ print('\nExample of line with next stop added:')
 print(net_data_grouped.loc[net_data_grouped['NOM_LINIA'] == 'D20'])
 
 weighted_net_data = net_data_grouped.merge(weights, on=['NOM_PARADA', 'PROXIMA_PARADA'])
-weighted_net_data.rename(
-    columns={'NOM_PARADA': 'source', 'PROXIMA_PARADA': 'target', 'NOM_LINIA': 'line', 'geometry': 'coordinates'},
+lines_net_data = weighted_net_data.groupby(['NOM_PARADA', 'PROXIMA_PARADA'])['NOM_LINIA'].agg(list).reset_index(
+    name='NOM_LINIA')
+net = lines_net_data.merge(weighted_net_data[['NOM_PARADA', 'PROXIMA_PARADA', 'weight', 'geometry']],
+                           on=['NOM_PARADA', 'PROXIMA_PARADA'], how='left')
+net.rename(
+    columns={'NOM_PARADA': 'source', 'PROXIMA_PARADA': 'target', 'NOM_LINIA': 'lines', 'geometry': 'coordinates'},
     inplace=True)
-weighted_net_data.drop(columns=['ORDRE', 'SENTIT'], inplace=True)
-print('\nExample of data with weights added:')
-print(weighted_net_data.head())
-print(weighted_net_data.columns)
+net.drop_duplicates(subset=['source', 'target'], inplace=True)
+# Remove self-loops
+net.drop(net[net['source'] == net['target']].index, inplace=True)
+net.to_pickle(os.path.join(data_folder, 'bus-bcn.pkl'))
 
-G = nx.from_pandas_edgelist(weighted_net_data, edge_attr=['weight'], create_using=DiGraph)
+print('\nExample of data with weights and list of lines added:')
+print(net.head())
+print(net.columns)
 
-attributes = {n: {'x': c.x, 'y': c.y} for n, c in zip(G.nodes, weighted_net_data['coordinates'])}
+G = nx.from_pandas_edgelist(net, edge_attr=['weight', 'lines'], create_using=DiGraph)
+
+attributes = {n: {'x': c.x, 'y': c.y} for n, c in zip(G.nodes, net['coordinates'])}
 nx.set_node_attributes(G, attributes)
 
 nx.write_pajek(G, os.path.join(data_folder, 'bus-bcn.net'))
-
--- Project/plot_network.py
+++ Project/plot_network.py
@@ -10,6 +10,6 @@ G = nx.read_pajek(os.path.join(data_folder, 'bus-bcn.net'))
 x_coord = nx.get_node_attributes(G, 'x')
 y_coord = nx.get_node_attributes(G, 'y')
 
-coordinates = {n: Point(x, y_coord[n]) for n, x in x_coord.items()}
-nx.draw(G, pos=coordinates, node_size=100)
+coordinates = {n: (x, y_coord[n]) for n, x in x_coord.items()}
+nx.draw(G, pos=coordinates, node_size=2)
 plt.show()
