9af5e3ddfd271fc7b4576ad859c273d1205546e0
Yazmina <yazmina.zurita@gmail.com>
2022-05-30 11:42:47 +0200

    Draw network with map

--- Project/plot_network.py
+++ Project/plot_network.py
@@ -1,14 +1,39 @@
 import os.path
-
+import geopandas
 import networkx as nx
 from matplotlib import pyplot as plt
+import igraph as ig
+from igraph import Graph
+from contextily import add_basemap
 
 data_folder = 'data'
-G = nx.read_pajek(os.path.join(data_folder, 'bus-bcn.net'))
+out_folder = "output"
+with_map = True
 
-x_coord = nx.get_node_attributes(G, 'x')
-y_coord = nx.get_node_attributes(G, 'y')
+if with_map:
+    addition = 0.005
+    g = Graph.Load(os.path.join(data_folder, "bus-bcn.net"), format="pajek")
+    net_data = geopandas.read_file(os.path.join(data_folder, "raw", "parades_linia.json"))
+    crs = net_data.crs.to_string()
+    del net_data
 
-coordinates = {n: (x, y_coord[n]) for n, x in x_coord.items()}
-nx.draw(G, pos=coordinates, node_size=2)
-plt.show()
+    fig, ax = plt.subplots(figsize=(10, 10))
+    ig.plot(g, target=ax, vertex_size=3, edge_width=0.5, edge_arrow_size=1.2)
+
+    ax.set_xlim(min(g.vs["x"]) - addition, max(g.vs["x"]) + addition)
+    ax.set_ylim(min(g.vs["y"]) - addition, max(g.vs["y"]) + addition)
+    ax.axis("off")
+    ax.set_aspect("auto")
+    fig.tight_layout()
+    add_basemap(ax, crs=crs)
+    plt.savefig(os.path.join(out_folder, "bus-bcn.png"))
+    plt.show()
+else:
+    G = nx.read_pajek(os.path.join(data_folder, 'bus-bcn.net'))
+
+    x_coord = nx.get_node_attributes(G, 'x')
+    y_coord = nx.get_node_attributes(G, 'y')
+
+    coordinates = {n: (x, y_coord[n]) for n, x in x_coord.items()}
+    nx.draw(G, pos=coordinates, node_size=2)
+    plt.show()
