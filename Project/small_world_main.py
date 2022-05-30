c65f62e0b445df5d076c3ca1e9e417adb788192a
Yazmina <yazmina.zurita@gmail.com>
2022-05-30 12:51:42 +0200

    Clean code

--- Project/small_world_main.py
+++ Project/small_world_main.py
@@ -1,14 +1,10 @@
 import os
 import pickle
-
 import networkx as nx
 import pandas as pd
 import small_world as sw
-import matplotlib.pyplot as plt
-import numpy as np
-
 from itertools import chain
-print(os.path.dirname(__file__))
+
 data_folder = 'data'
 out_folder = 'output'
 absolute_out_folder = os.path.join(os.path.dirname(__file__), out_folder)
@@ -26,14 +22,10 @@ line_names = list(set(chain(*df['lines'].tolist())))
 f = open(os.path.join(os.path.dirname(__file__), data_folder, 'line-connections.pkl'), 'rb')
 connections = pickle.load(f)
 f.close()
-f = open(os.path.join(os.path.dirname(__file__), out_folder, 'transhipment_dict.pkl'), 'rb')
-transhipment_dict = pickle.load(f)
-f.close()
-f = open(os.path.join(os.path.dirname(__file__), out_folder, 'path_length_dict.pkl'), 'rb')
-path_length_dict = pickle.load(f)
-f.close()
+
+# WARNING: when setting load=False, the program will execute methods that take a very long time to finish
 sw.plot_diameter_N(G, connections, out_folder=absolute_out_folder, load=True)
-sw.plot_shortest_routes(transhipment_dict, path_length_dict, out_folder=absolute_out_folder, load=True)
+sw.plot_shortest_routes(G, lines, out_folder=absolute_out_folder, load=True)
 sw.plot_hists(G, out_folder=absolute_out_folder)
 sw.small_word_stats(G, out_folder=absolute_out_folder)
 
